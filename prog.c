7781dea49acd8093f7f98be26cdd1f17bc159824
Blake Godwin <bgodwi00@g.uafs.edu>
2022-01-19 23:08:04 +0000

    First Update

---
+++ prog.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#define SIZE 5
+
+//*********** Function Prototypes*******
+void displayArray(int array[], int size);
+
+int main(int argc, char *argv[]){
+   int numbers[SIZE]={10,34,53,68,21};
+   displayArray(numbers, SIZE);
+   return 0;
+}
+
+void displayArray(int array[], int size){
+   int index;
+   putchar('[');
+   for(index = 0; index < size;index++){
+       if(index!=0){
+	       putchar(',');
+       }
+	   printf("%d",array[index]);
+   }
+   putchar(']');
+   putchar('\n');
+}
